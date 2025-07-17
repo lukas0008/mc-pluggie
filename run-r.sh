acc2b4a97f7ebc3bc8e38c480ffc396f46f18ad6
lukas0008 <lukzaj333@gmail.com>
2025-07-17 14:18:26 +0200

    omfg

--- run-r.sh
+++ run-r.sh
@@ -1,10 +1,16 @@
 cargo build -r -p mc-network --features mc-network/init
 cargo build -r -p mc-tick --features mc-tick/init
 cargo build -r -p mc-example --features mc-example/init
+cargo build -r -p mc-loader --features mc-loader/init
+cargo build -r -p mc-status --features mc-status/init
+cargo build -r -p mc-registry --features mc-registry/init
 
-cp ./target/release/libmc_network.so ../pluggie/plugins/
+cp ./target/release/libmc_network.so ../pluggie/plugins
 cp ./target/release/libmc_tick.so ../pluggie/plugins
 cp ./target/release/libmc_example.so ../pluggie/plugins
+cp ./target/release/libmc_loader.so ../pluggie/plugins
+cp ./target/release/libmc_status.so ../pluggie/plugins
+cp ./target/release/libmc_registry.so ../pluggie/plugins
 
 cd ~/Code/rust/pluggie
 cargo run -r
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cargo build --features init
-cp ./target/debug/libmc_network.so ../pluggie/plugins/
+cp ./target/debug/libmc_network.so ../pluggie/plugins
 cp ./target/debug/libmc_tick.so ../pluggie/plugins
 cp ./target/debug/libmc_example.so ../pluggie/plugins
 cp ./target/debug/libmc_loader.so ../pluggie/plugins
